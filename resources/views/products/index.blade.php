99a2946dc93aa5c1407921fbe2cafeb6c3140fe6
Efrain <carlosefrainchallo1@gmail.com>
2025-04-19 13:09:56 -0500

    Se agrega la interfaz index (interfaz pricnipal)

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11 - Crud</title>
 </head>
+
 <body>
     <h1>Listado de productos</h1>
+    <table>
+        <tr>
+            <th>id</th>
+            <th>Nomnbre</th>
+            <th>Precio</th>
+            <th>Descripcion</th>
+        </tr>
+        @foreach ($products as $product)
+            <tr>
+                <td>{{$product->id}}</td>
+                <td>{{$product->name}}</td>
+                <td>{{$product->price}}</td>
+                <td>{{$product->description}}</td>
+            </tr>
+        @endforeach
+    </table>
 </body>
+
 </html>
